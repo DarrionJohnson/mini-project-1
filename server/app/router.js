d1c71fd4051530fce31c695491922ed6b7ebb410
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-11-05 14:42:53 -0500

    Import config and assign it to var collection.

--- server/app/router.js
+++ server/app/router.js
@@ -1,6 +1,11 @@
 import Router from "express";
+import config from "./config.js";
+import client from "./loader.js";
 
 const router = new Router();
+const collection = client
+  .db(config.db.name)
+  .collection(config.db.collectionName);
 
 router.get("/", (_, res) => {
   res.send("Localhost:3000/current-listings router Test");
