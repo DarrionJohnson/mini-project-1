b90392edede04502c1888b6ce0b2f34aab6d1ef3
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-11-04 19:42:12 -0500

    Add test router.

--- server/app/router.js
+++ server/app/router.js
@@ -2,6 +2,8 @@ import Router from "express";
 
 const router = new Router();
 
-// TODO: Add routes here (maybe 🤔 start with a GET test route)
+router.get("/", (_, res) => {
+  res.send("Localhost:3000/api router Test");
+});
 
 export default router;
