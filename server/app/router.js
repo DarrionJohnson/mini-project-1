ef8b54ae2c2412de64628dbcb8d2f92f03200a3b
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-11-07 22:57:07 -0600

    Change :id to :number for limit amount.

--- server/app/router.js
+++ server/app/router.js
@@ -11,8 +11,8 @@ router.get("/", (_, res) => {
   res.send("Localhost:3000/current-listings router Test");
 });
 
-router.get("/limit/:id", async (req, res) => {
-  const num = Number(req.params.id);
+router.get("/limit/:number", async (req, res) => {
+  const num = Number(req.params.number);
   const list = await collection.find({}).limit(num).toArray();
   res.json(list);
 });
