d6de041cdeefaefeb2d5aa3e837001d878411e2e
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-11-13 21:52:06 -0600

    Create a multi uses router for current listings

--- server/app/router.js
+++ server/app/router.js
@@ -8,9 +8,54 @@ const collection = client
   .db(config.db.name)
   .collection(config.db.collectionName);
 
-router.get("/", async (_, res) => {
-  const display = await collection.find({}).limit(20).toArray();
-  res.send(display);
+// Routes for current-listings
+// Limit Check
+// Standard Outcomes Check
+// ID Check
+// Query Check
+
+router.get("/", async (req, res) => {
+  const queries = Object.keys(req.query);
+  const values = Object.values(req.query);
+
+  console.log(`Here are the Request paramaters: ${queries[0]}; ${values[0]}`);
+
+  if (queries[0] === "limit") {
+    console.log("limit set");
+
+    const num = Number(values);
+
+    if (num >= 1) {
+      const display = await collection.find({}).limit(num).toArray();
+
+      res.send(display);
+    } else {
+      res.send(
+        "Please Enter a valid number; higher than 0; for the limit amount."
+      );
+    }
+  } else if (queries[0] === undefined) {
+    console.log("No queries ");
+
+    const display = await collection.find({}).limit(50).toArray();
+    res.send(display);
+  } else if (queries[0] === `id` || queries[0] === `_id`) {
+    console.log(`Id was quired.`);
+
+    const display = await collection
+      .find({ _id: { $regex: values[0], $options: "i" } })
+      .toArray();
+
+    res.send(display);
+  } else {
+    console.log(`Query: ${queries[0]}; Was quired.`);
+
+    const display = await collection
+      .find({ [queries[0]]: { $regex: values[0], $options: "i" } })
+      .toArray();
+
+    res.send(display);
+  }
 });
 
 // router.get("/", async (_, res) => {
